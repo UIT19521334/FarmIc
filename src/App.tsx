83f908b45747afff583fc296fa9270e044be5e6a
NDCDat <19521334@gm.uit.edu.vn>
2023-12-28 10:32:06 +0700

    [Update] update auto router

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+import RouterRoot from 'react-native-auto-route';
+
+const App = () => {
+  return <RouterRoot />;
+};
+
+export default App;
